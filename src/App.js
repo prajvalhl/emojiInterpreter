5c0ad8fbd181c4c7a549ff9da736464970a6ba39
Prajval <prajvalhl@gmail.com>
2021-08-02 22:03:47 +0530

    added more emoji

--- src/App.js
+++ src/App.js
@@ -1,41 +1,135 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-const emojiDict = {
-  "🍕": "Pizza",
-  "🍔": "Hamburger",
-  "🍟": "French Fries",
-  "🍿": "Popcorn",
+const emojiDatabase = {
+  "🍇": "Grapes",
+  "🍈": "Melon",
+  "🍉": "Watermelon",
+  "🍊": "Tangerine",
+  "🍋": "Lemon",
+  "🍌": "Banana",
+  "🍍": "Pineapple",
+  "🍎": "Red Apple",
+  "🍏": "Green Apple",
+  "🍐": "Pear",
+  "🍑": "Peach",
+  "🍒": "Cherries",
+  "🍓": "Strawberry",
+  "🥝": "Kiwi Fruit",
+  "🍅": "Tomato",
+  "🥥": "Coconut",
+  "🥑": "Avocado",
+  "🍆": "Eggplant",
+  "🥔": "Potato",
+  "🥕": "Carrot",
+  "🌽": "Ear of Corn",
+  "🌶️": "Hot Pepper",
+  "🥒": "Cucumber",
+  "🥦": "Broccoli",
+  "🍄": "Mushroom",
+  "🥜": "Peanuts",
+  "🌰": "Chestnut",
+  "🍞": "Bread",
+  "🥐": "Croissant",
+  "🥖": "Baguette Bread",
+  "🥨": "Pretzel",
   "🧇": "Waffle",
   "🥞": "Pancakes",
-  "🥐": "Croissant",
+  "🧀": "Cheese Wedge",
+  "🍖": "Meat on Bone",
+  "🍗": "Poultry Leg",
+  "🥩": "Cut of Meat",
+  "🥓": "Bacon",
+  "🍔": "Hamburger",
+  "🍟": "French Fries",
+  "🍕": "Pizza",
+  "🌭": "Hot Dog",
+  "🥪": "Sandwich",
+  "🌮": "Taco",
+  "🌯": "Burrito",
+  "🥙": "Stuffed Flatbread",
+  "🥚": "Egg",
+  "🍳": "Cooking",
+  "🥘": "Shallow Pan of Food",
+  "🍲": "Pot of Food",
+  "🥣": "Bowl with Spoon",
   "🥗": "Green Salad",
-  "🥪": "Sandwitch",
-  "🌮": "Taco"
+  "🍿": "Popcorn",
+  "🥫": "Canned Food",
+  "🍱": "Bento Box",
+  "🍘": "Rice Cracker",
+  "🍙": "Rice Ball",
+  "🍚": "Cooked Rice",
+  "🍛": "Curry Rice",
+  "🍜": "Steaming Bowl",
+  "🍝": "Spaghetti",
+  "🍠": "Roasted Sweet Potato",
+  "🍢": "Oden",
+  "🍣": "Sushi",
+  "🍤": "Fried Shrimp",
+  "🍥": "Fish Cake with Swirl",
+  "🍡": "Dango",
+  "🥟": "Dumpling",
+  "🥠": "Fortune Cookie",
+  "🥡": "Takeout Box",
+  "🍦": "Soft Ice Cream",
+  "🍧": "Shaved Ice",
+  "🍨": "Ice Cream",
+  "🍩": "Doughnut",
+  "🍪": "Cookie",
+  "🎂": "Birthday Cake ",
+  "🍰": "Shortcake ",
+  "🥧": "Pie ",
+  "🍫": "Chocolate Bar ",
+  "🍬": "Candy ",
+  "🍭": "Lollipop ",
+  "🍮": "Custard ",
+  "🍯": "Honey Pot ",
+  "🍼": "Baby Bottle ",
+  "🥛": "Glass of Milk ",
+  "☕": "Hot Beverage ",
+  "🍵": "Teacup Without Handle ",
+  "🍶": "Sake ",
+  "🍾": "Bottle with Popping Cork ",
+  "🍷": "Wine Glass ",
+  "🍸": "Cocktail Glass ",
+  "🍹": "Tropical Drink ",
+  "🍺": "Beer Mug ",
+  "🍻": "Clinking Beer Mugs ",
+  "🥂": "Clinking Glasses ",
+  "🥃": "Tumbler Glass ",
+  "🥤": "Cup with Straw ",
+  "🥢": "Chopsticks ",
+  "🍽️": "Fork and Knife with ",
+  "🍴": "Fork and Knife ",
+  "🥄": "Spoon "
 };
 
-const emojiArr = Object.keys(emojiDict);
+const emojiArr = Object.keys(emojiDatabase);
 
 export default function App() {
   const [userInput, setUserInput] = useState("");
 
   function emojiInputHandler(event) {
     let emojiInput = event.target.value;
-    if (emojiInput in emojiDict) {
-      setUserInput(emojiDict[emojiInput]);
+    if (emojiInput in emojiDatabase) {
+      setUserInput(emojiDatabase[emojiInput]);
     } else {
       setUserInput("Sorry! We don't have that emoji in our database!");
     }
   }
 
   function emojiClickHandler(emoji) {
-    setUserInput(emojiDict[emoji]);
+    setUserInput(emojiDatabase[emoji]);
   }
 
   return (
     <div className="App">
-      <h1>What's this Food Emoji?</h1>
-      <input onChange={emojiInputHandler}></input>
+      <h1>What's this Food / Drink Emoji?</h1>
+      <input
+        onChange={emojiInputHandler}
+        placeholder="Enter a food/drink emoji here OR Choose one from below"
+      ></input>
 
       <h2>{userInput}</h2>
 
